f7dc5ac6e2ab6d498cb328847b6e6f351a5c012d
Nguyen <vuxuannguyen11122001@gmail.com>
2023-06-14 01:54:35 +0700

    feat: Add Disease Prediction from Symptoms

--- app/main.py
+++ app/main.py
@@ -1,7 +1,26 @@
-from flask import Flask
+from flask import Flask, request,jsonify
+import pandas as pd
+import numpy as np
+from joblib import load
+
 
 app = Flask(__name__)
 
 @app.route("/")
 def home_view():
         return "<h1>Hello World!</h1>"
+@app.route('/submit', methods=['POST'])
+def submit():
+    data = request.get_json()
+    df_test = pd.DataFrame(columns=list(data.keys()))
+    
+    df_test.loc[0] = np.array(list(data.values()))
+
+    # Load pre-trained model
+    clf = load(str("./saved_model/random_forest.joblib"))
+    
+    
+    predicted=clf.predict(df_test)
+    result = {'result':predicted.item(0)}
+    
+    return jsonify(result)
